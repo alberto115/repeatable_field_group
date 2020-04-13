50f62781d4322aac5db6e4b2f58b663063008df7
Alberto Contreras <a.contreras@catchdigital.com>
2020-04-13 10:22:23 +0100

    Initial commit of repeatable field group.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Repeatable field group
+
+Drupal 8 module that provides a field group formatter to group multi-value fields.
+
+## Features
+
+This module group the repeatable fields by value index into a single details container.
+
+If the fields are set unlimited values, an add more fields will be use.
+
+## Dependencies
+
+- field_group
+
+## Known issues
+
+- There is no current integration with core media gallery fields.
+- Fields grouped must have the same cardinality.
---
+++ src/Plugin/field_group/FieldGroupFormatter/RepeatableFieldGroup.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\repeatable_field_group\Plugin\field_group\FieldGroupFormatter;
+
+use Drupal\field_group\FieldGroupFormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\NestedArray;
+
+/**
+ * Repatable field group element.
+ *
+ * @FieldGroupFormatter(
+ *   id = "repeatable_field_group",
+ *   label = @Translation("Repeatable field group"),
+ *   description = @Translation("Group together fields with infinite values."),
+ *   supported_contexts = {
+ *     "form"
+ *   }
+ * )
+ */
+class RepeatableFieldGroup extends FieldGroupFormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function process(&$element, $processed_object) {
+    // Set settings for opening and closing
+    $element += [
+      '#type' => 'details',
+      '#title' => $this->getLabel(),
+      '#description' => $this->getSetting('description'),
+      '#open' => TRUE,
+      '#items_count' => 0
+    ];
+
+    if ($this->getSetting('id')) {
+      $element['#id'] = Html::getUniqueId($this->getSetting('id'));
+    }
+
+    $classes = $this->getClasses();
+    if (!empty($classes)) {
+      $element += [
+        '#attributes' => ['class' => $classes],
+      ];
+    }
+
+    if ($this->getSetting('required_fields')) {
+      $element['#attached']['library'][] = 'field_group/formatter.details';
+      $element['#attached']['library'][] = 'field_group/core';
+    }
+  }
+
+  public static function addMore(array $form, FormStateInterface $form_state) {
+
+    $button = $form_state->getTriggeringElement();
+    // Go one level up in the form, to the widgets container.
+    $element = NestedArray::getValue($form, array_slice($button['#array_parents'], 0, -1));
+    $group = $button['#group'];
+
+    // Add one more of each field
+    foreach($button['#fields_parents'] as $field_name => $parents) {
+      $field_state = \Drupal\Core\Field\WidgetBase::getWidgetState($parents, $field_name, $form_state);
+      $field_state['items_count']++;
+      \Drupal\Core\Field\WidgetBase::setWidgetState($parents, $field_name, $form_state, $field_state);
+      $form_state->setRebuild();
+    }
+  }
+
+  /**
+   * Ajax callback for the "Add another item" button.
+   *
+   * This returns the new page content to replace the page content made obsolete
+   * by the form submission.
+   */
+  public static function addMoreAjax(array $form, FormStateInterface $form_state) {
+    $button = $form_state->getTriggeringElement();
+    // Go one level up in the form, to the widgets container.
+    $element = NestedArray::getValue($form, array_slice($button['#array_parents'], 0, -1));
+    return $element;
+  }
+
+}
